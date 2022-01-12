1ec16cee3fd3025898b34c319229a4b78abc75ab
Cameron Beamon <BeamonCameron@gmail.com>
2022-01-11 18:51:18 -0600

    added features to controller file

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,16 @@ class ProductsController < ApplicationController
   def show_all
     render json: Product.all
   end
+
+  def show_first
+    render json: Product.first
+  end
+
+  def show_second
+    render json: Product.second
+  end
+
+  def show_third
+    render json: Product.third
+  end
 end
