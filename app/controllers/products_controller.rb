aee23620b01706713888272c2bbee21965486cc7
Cameron Beamon <BeamonCameron@gmail.com>
2022-01-11 18:45:50 -0600

    added a route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,5 @@
 class ProductsController < ApplicationController
+  def show_all
+    render json: Product.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "/all_products", controller: "products", action: "show_all"
   # Defines the root path route ("/")
   # root "articles#index"
 end
