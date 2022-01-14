b70f59f2a5e33453c54ea27131b8160044f3e365
Cameron Beamon <BeamonCameron@gmail.com>
2022-01-13 19:53:45 -0600

    fixed controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/all_products" => "products#index"
   post "/all_products/" => "products#create"
-  post "/all_products/:id" => "products#show"
-  patch "all_products/:id" => "products#update"
+  get "/all_products/:id" => "products#show"
+  patch "/all_products/:id" => "products#update"
   delete "/all_products/:id" => "products#destroy"
   # get "/first_product", controller: "products", action: "show_first"
   # get "/third_product", controller: "products", action: "show_third"
