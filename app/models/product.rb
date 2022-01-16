d214d852759e926ee3e473da20dd80cbb5ac6f63
Cameron Beamon <BeamonCameron@gmail.com>
2022-01-16 10:11:46 -0600

    added methods to model to learn how to use them

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,20 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    if price < 10
+      return true
+    else
+      return false
+    end
+  end
+
+  def tax
+    tax_cost = price * 0.09
+    @tax_cost = tax_cost
+    return tax_cost
+  end
+
+  def total
+    total_cost = price + @tax_cost
+    return total_cost
+  end
 end
