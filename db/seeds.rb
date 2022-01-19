2c3fee0b7181bb11042abc2b9a3f9c47c829d4ad
Cameron Beamon <BeamonCameron@gmail.com>
2022-01-18 18:33:56 -0600

    added an image controller so each product can have multiple images

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,12 @@ supplier = Supplier.create(name: "Apple", email: "apple.com", phone_number: "123
 supplier = Supplier.create(name: "Google", email: "google.com", phone_number: "765432")
 supplier = Supplier.create(name: "Samsung", email: "samsung.com", phone_number: "4345345")
 
-product = Product.create(name: "Iphone", price: 900, image_url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-pro-max-graphite-select?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1631652956000", description: "Elegant", supplier_id: 1)
-product = Product.create(name: "Android", price: 1000, image_url: "https://images.idgesg.net/images/article/2020/08/android-awkward-timing-100855433-large.jpg?auto=webp&quality=85,70", description: "Robust", supplier_id: 1)
-product = Product.create(name: "Blackberry", price: 20, image_url: "https://ih1.redbubble.net/image.1849445138.4826/st,small,507x507-pad,600x600,f8f8f8.jpg", description: "Absent", supplier_id: 2)
+product = Product.create(name: "Iphone", price: 900, description: "Elegant", supplier_id: 1)
+product = Product.create(name: "Android", price: 1000, description: "Robust", supplier_id: 1)
+product = Product.create(name: "Blackberry", price: 20, description: "Absent", supplier_id: 2)
+
+image = Image.create(product_id: 1, url: "https://ih1.redbubble.net/image.1849445138.4826/st,small,507x507-pad,600x600,f8f8f8.jpg")
+image = Image.create(product_id: 1, url: "https://images.idgesg.net/images/article/2020/08/android-awkward-timing-100855433-large.jpg?auto=webp&quality=85,70")
+image = Image.create(product_id: 1, url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-pro-max-graphite-select?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1631652956000")
 
 puts "Done!"
