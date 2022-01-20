bf1ab1d625dd65561eef8c6376c6abf01de9cf6f
Cameron Beamon <BeamonCameron@gmail.com>
2022-01-19 18:57:29 -0600

    learned how to save user login in info with bcrypt

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  post "/users" => "users#create"
   get "/all_products" => "products#index"
   post "/all_products" => "products#create"
   get "/all_products/:id" => "products#show"
   patch "/all_products/:id" => "products#update"
   delete "/all_products/:id" => "products#destroy"
-  get "/all_supplier" => "suppliers#index"
+  get "/all_suppliers" => "suppliers#index"
   post "/all_suuppliers/" => "suppliers#create"
   get "/all_suuppliers/:id" => "suppliers#show"
   patch "/all_suuppliers/:id" => "suppliers#update"
